4e3ea42f7f37d1cff7b805afe950e13da82246ef
Alena <alenakrivohiza@gmail.com>
2019-03-08 23:36:44 +0300

    Update gulpfile.js and install new package

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,20 @@ var
 	pug 					= require('gulp-pug'),
 	browserSync		= require('browser-sync'),
 	plumber 			= require('gulp-plumber'),
+	sass 					= require('gulp-sass'),
+	cssToScss 		= require('gulp-css-scss'),
 
 	reload				= browserSync.reload; 
+
+gulp.task('cssToScss', () => {
+	return gulp.src([
+		'app/libs/bootstrap/dist/css/bootstrap-grid.min.css',
+		// 'app/libs/magnific-popup/dist/magnific-popup.css',
+		// 'app/libs/animate.css/animate.min.css'
+		])
+	.pipe(cssToScss())
+	.pipe(gulp.dest('app/libs/cssToScss'));
+});	
 
 gulp.task('pug', () => {
 	return gulp.src(
@@ -16,9 +28,20 @@ gulp.task('pug', () => {
 	.pipe(reload({stream: true}))	
 });
 
-//---------------------------------------------
-// Browser-Sync
-//---------------------------------------------
+gulp.task('sass', () => { 	
+	return gulp.src('app/sass/**/*.sass')		
+		.pipe(sass({
+			// outputStyle: 'expand', 
+			// includePaths: require('node-bourbon').includePaths
+		}).on('error', sass.logError)) // Оповещение в случае ошибки при компиляции SASS в CSS
+		// .pipe(autoprefixer(['last 15 versions'])) // Добавление автопрефиксов, для одинакового отображения во всех браузерах (последнии 15 версий)
+		// .pipe(gulp.dist('dist/css'))		
+		// .pipe(csso())	// Минимизируем	 		
+		// .pipe(rename({suffix: '.min', prefix : ''})) // Добавление суффикса и префикса в название CSS файла
+		.pipe(gulp.dest('dist/css'))			
+		.pipe(browserSync.stream()); // Inject	
+});
+
 gulp.task('browser-sync', () => { 
 	browserSync({ 
 		server: {baseDir: 'dist'},
@@ -26,8 +49,9 @@ gulp.task('browser-sync', () => {
 	});
 });
 
-gulp.task('watch', ['browser-sync'], () => {
+gulp.task('watch', ['pug', 'sass', 'browser-sync'], () => {
 	gulp.watch('app/pug/index.pug', ['pug']);
+	gulp.watch('app/sass/*.sass', ['sass']);
 });	
 
 
