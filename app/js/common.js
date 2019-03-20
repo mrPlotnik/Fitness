1d44cc465c88854dc482e4b9f1f7a3b45ee8064f
AlenkaKR <alenakrivohiza@gmail.com>
2019-03-20 18:18:27 +0300

    Adaptability

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
-	$("#sec-3 .item-descr h3").equalHeights();	
-	$("#sec-3 .item-descr p").equalHeights();	
+	resize();
+
+	$(window).resize(function() {
+		resize();
+	});
+
+	function resize() {
+		$("#sec-3 .item-descr h3").equalHeights();	
+		$("#sec-3 .item-descr p").equalHeights();	
+		console.log('Done!');
+	};	
 
 }); 
