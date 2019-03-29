cb02491989569dc048bdeb17db31f5f744bc6aad
alenkakr <alenakrivohiza@gmail.com>
2019-03-30 00:51:58 +0700

    Update common.js

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 	function resize() {
 		$("#sec-3 .item-descr h3").height('auto').equalHeights();	
 		$("#sec-3 .item-descr p").height('auto').equalHeights();	
-		console.log('done!');
+		// $("#sec-2-2 h3").height('auto').equalHeights();	
+		// $("#sec-2-2 .img-container p").height('auto').equalHeights();	
+		
 	};
 
 	$(window).resize(function() {
