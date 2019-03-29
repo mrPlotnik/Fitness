460de23c726cd9bde80fd90d097118a192dfb9b3
alenkakr <alenakrivohiza@gmail.com>
2019-03-29 15:16:42 +0700

    Доработка

--- app/js/common.js
+++ app/js/common.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
 	resize();
 
+	function resize() {
+		$("#sec-3 .item-descr h3").height('auto').equalHeights();	
+		$("#sec-3 .item-descr p").height('auto').equalHeights();	
+		console.log('done!');
+	};
+
 	$(window).resize(function() {
 		resize();
 	});
-
-	function resize() {
-		$("#sec-3 .item-descr h3").equalHeights();	
-		$("#sec-3 .item-descr p").equalHeights();	
-		console.log('Done!');
-	};	
 
 }); 
