5dc367f7bf3dcb825864760b9b2dbaba48bb7e61
AlenkaKR <alenakrivohiza@gmail.com>
2019-04-05 23:30:45 +0900

    Переименование секций, файлов, классов

--- app/js/common.js
+++ app/js/common.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	resize();
 
 	function resize() {
-		$("#sec-3 .item-descr h3").height('auto').equalHeights();	
-		$("#sec-3 .item-descr p").height('auto').equalHeights();	
+		$("#sec-4 .item-descr h3").height('auto').equalHeights();	
+		$("#sec-4 .item-descr p").height('auto').equalHeights();	
 		// $("#sec-2-2 h3").height('auto').equalHeights();	
 		// $("#sec-2-2 .img-container p").height('auto').equalHeights();	
 		
