7f3649955b8be465e2d7551921d0d1e7fcf04272
AlenkaKR <alenakrivohiza@gmail.com>
2019-04-07 23:53:41 +0900

    Работа над формой регистрации. Добавлен плагин Bootstrap validator. Данные из формы отправляются через AJAX JQuery в PHP Mail на почту. Добалена анимация формы

--- app/js/common.js
+++ app/js/common.js
@@ -13,37 +13,59 @@ $(document).ready(function() {
 
 	$("a[href*='#']").mPageScroll2id();
 
-	$("#Form").validator().on("submit", (function(event) {
-		if (event.isDefaultPrevented()) {
-      // обработка ошибки формы...
-    } else {
-      // все в порядке!      
 
-	    // отменяет отправку данных формы
-	    event.preventDefault();
-	    submitForm();
-    }
-	});
 
-	function submitForm(){
-    // Инициируем переменную с содержимым формы
-    var name = $("#name").val();
-    var email = $("#email").val();   
 
-    $.ajax({
-      type: "POST",
-      url: "php/form-process.php",
-      data: "name=" + name + "&email=" + email,
-      success : function(text){
-        if (text == "success"){
-          formSuccess();
-        }
-      }
+	$("#form").validator().on("submit", (function(event) {
+		if (event.isDefaultPrevented()) {			
+			formError();
+			submitMSG(false, "Заполните всю форму!");
+		} else {
+			// все в порядке!      
+			
+			event.preventDefault(); // Останавливает отправку данных формы при обновлении страницы без выбора действия в форме.
+			submitForm();
+		}
+	}));
+
+	function submitForm() {
+		// Инициируем переменную с содержимым формы
+		var name = $("#name").val();
+		var email = $("#email").val();   
+
+		$.ajax({
+			type: "POST",
+			url: "php/form-process.php",
+			data: "name=" + name + "&email=" + email,
+			success : function(text) {
+				if (text == "success") {
+					formSuccess();
+				}
+			}
+		})
+	};
+
+	function formSuccess() {
+		$("#msgSubmit").removeClass("hidden");
+		$("#form")[0].reset();
+		submitMSG(true, "Сообщение отправлено!")
+	};
+
+	function formError(){
+    $("#form").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass();
     });
+	};
+
+	function submitMSG(valid, msg){
+      var msgClasses;
+    if(valid){
+      msgClasses = "tada animated text-success text-uppercase";
+    } else {
+      msgClasses = "w-100 text-danger text-uppercase";
+    }
+    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 	}
 
-	function formSuccess(){
-    $("#msgSubmit").removeClass("hidden");
-	}
 
 }); 
