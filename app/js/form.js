73009ae80d6fa24f68478c0e78ca4b301f7938b3
AlenkaKR <alenakrivohiza@gmail.com>
2019-04-18 23:05:41 +0900

    Доработка формы

--- app/js/form.js
+++ app/js/form.js
@@ -1,51 +1,76 @@
+// Этот фрагмент кода прослушивает функции отправки данных #form. Перед этой функцией мы обрабатываем переменную event, которая хранит действие отправки данных формы для функции.
+
+// Этот фрагмент кода проверяет, нашел ли Bootstrap Validator проблемы и остановил ли работу кода. 
 $("#form").validator().on("submit", (function(event) {
-		if (event.isDefaultPrevented()) {			
+		if (event.isDefaultPrevented()) {	// обработка ошибки формы...	
 			formError();
 			submitMSG(false, "Заполните всю форму!");
-		} else {
-			// все в порядке!      
-			
+		} else { // если все в порядке! 
 			event.preventDefault(); // Останавливает отправку данных формы при обновлении страницы без выбора действия в форме.
-			submitForm();
+			submitForm(); // вызывает функцию submitForm(), которая отправляет данные формы в php/form.php
 		}
-	}));
+}));
 
-	function submitForm() {
-		// Инициируем переменную с содержимым формы
-		var name = $("#name").val();
-		var email = $("#email").val();   
-		var phone = $("#phone").val();   
-
-		$.ajax({
-			type: "POST",
-			url: "php/form.php",
-			data: "name=" + name + "&email=" + email + 'phone', // Ошибка?
-			success : function(text) {
-				if (text == "success") {
-					formSuccess();
-				}
+function submitForm() {
+	// инициируем переменную с содержимым формы
+	var name = $("#name").val();
+	var email = $("#email").val();   
+	var phone = $("#phone").val();   
+	// инициируем объект AJAX внутри
+	$.ajax({
+		type: "POST",
+		url: "php/form.php", // адрес размещения файла PHP
+		data: "name=" + name + "&email=" + email + 'phone', // ошибка?
+		// функция обратного вызова
+		success : function(text) { // если запрос успешен вызываем функцию
+			if (text == "success") { // проверяем на соответствие статуса запроса
+				formSuccess(); // если, статус запроса == "success", то вызоваем функцию formSuccess(),
 			}
-		})
-	};
+		}
+	})
+};
 
-	function formSuccess() {
-		$("#msgSubmit").removeClass("hidden");
-		$("#form")[0].reset();
-		submitMSG(true, "Сообщение отправлено!")
-	};
+function formSuccess() {
+	$("#msgSubmit").removeClass("hidden");
+	$("#form")[0].reset();
+	submitMSG(true, "Сообщение отправлено!")
+};
 
-	function formError(){
-    $("#form").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass();
-    });
-	};
+function formError(){
+	$("#form").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		$(this).removeClass();
+	});
+};
 
-	function submitMSG(valid, msg){
-      var msgClasses;
-    if(valid){
-      msgClasses = "tada animated text-success text-uppercase";
-    } else {
-      msgClasses = "w-100 text-danger text-uppercase";
-    }
-    $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+function submitMSG(valid, msg){
+		var msgClasses;
+	if(valid){
+		msgClasses = "tada animated text-success text-uppercase";
+	} else {
+		msgClasses = "w-100 text-danger text-uppercase";
 	}
+	$("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+
+	// Поведение label
+	$("input").blur( function() {
+
+		var findLabel 	= $(this).parent().find("label"),
+				inputValue	= $(this).val(),
+				active 			= "label-active",
+				notActive 	= "label-not-active";				
+
+		if(inputValue == '') {
+			findLabel.addClass(notActive);
+			findLabel.removeClass(active);	
+			console.log(inputValue + " notActive"); // Удалить потом
+		}   
+	 
+		else {
+			findLabel.addClass(active);
+			findLabel.removeClass(notActive);
+			console.log(inputValue + " active"); // Удалить потом
+		}
+		 
+	});	
