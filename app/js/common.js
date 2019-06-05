e5ca8c3b13d6a3e7a21d1e26088f63b20a05664c
AlenkaKR <alenakrivohiza@gmail.com>
2019-06-05 22:55:44 +0900

    Добавлен блок с отзывами

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 
+	//////////////////////////////////////////////////////
+	// Одинаковая высота блоков
+	//////////////////////////////////////////////////////
 	resize();	
 
 	$(window).resize(function() {
 		resize();
 	});
-
+	
 	function resize() {
 		$("#sec-4 .item-descr h3").height('auto').equalHeights();	
 		$("#sec-4 .item-descr p").height('auto').equalHeights();	
 	};
 
+	//////////////////////////////////////////////////////
+	// Плавный скролл
+	//////////////////////////////////////////////////////
 	$("a[href*='#']").mPageScroll2id();
 
+	//////////////////////////////////////////////////////
+	// magnific-popup
+	//////////////////////////////////////////////////////
 	$('.popup-gallery').magnificPopup({
 		delegate: 'a',
 		type: 'image',
@@ -31,15 +40,18 @@ $(document).ready(function() {
 		}
 	});
 
+	//////////////////////////////////////////////////////
+	// Countdown start
+	//////////////////////////////////////////////////////
 	function getTimeRemaining(endtime) {
 		var t = Date.parse(endtime) - Date.parse(new Date());
-		var seconds = Math.floor((t / 1000) % 60);
-		var minutes = Math.floor((t / 1000 / 60) % 60);
-		var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-		var days = Math.floor(t / (1000 * 60 * 60 * 24));
+		var seconds = Math.floor((t / 1000) % 55);
+		var minutes = Math.floor((t / 1000 / 55) % 20);
+		var hours = Math.floor((t / (1000 * 55 * 20)) % 24);
+		// var days = Math.floor(t / (1000 * 60 * 60 * 24));
 		return {
 			'total': t,
-			'days': days,
+			// 'days': days,
 			'hours': hours,
 			'minutes': minutes,
 			'seconds': seconds
@@ -70,7 +82,88 @@ $(document).ready(function() {
 		var timeinterval = setInterval(updateClock, 1000);
 	}
 	 
-	var deadline = new Date(Date.parse(new Date()) + 1 * 24 * 60 * 60 * 1000); // for endless timer
+	var deadline = new Date(Date.parse(new Date()) + 7 * 20 * 55 * 1000); // for endless timer
 	initializeClock('clock', deadline);
-	
+
+	//////////////////////////////////////////////////////
+	// Слайдер с отзывами
+	//////////////////////////////////////////////////////
+	var blockWidth = $('.cont').width();
+
+	function resize() {
+		var newBlockWidth = $('.cont').width();
+
+		if (blockWidth != newBlockWidth) {
+			
+			blockWidth = newBlockWidth;
+
+			$('.slides>li').width(blockWidth);
+			$('.slides').width(blockWidth*$('.slides>li').length);
+
+			$('.slides').css('left',-blockWidth);
+			$('.slides>li:last-child').prependTo('.slides');		
+
+		}	
+	}
+
+	$(window).resize(function() {
+		resize();		
+	});
+
+	// Кнопка "вперед"
+	function nextSlide() {
+		$('.slides').animate( {
+			marginLeft: -blockWidth
+		}, 500, function() {
+			$('.slides>li:first-child').appendTo('.slides');
+			$('.slides').css('margin-left', 0);
+		});
+	}
+
+		// Кнопка "назад"
+	function prevSlide() {
+		$('.slides').animate( {
+			marginLeft: blockWidth
+		}, 500, function() {
+			$('.slides>li:last-child').prependTo('.slides');
+			$('.slides').css('margin-left', 0);
+		});
+	}
+
+	$('.next').click(nextSlide);
+	$('.prev').click(prevSlide);
+
+	//////////////////////////////////////////////////////	
+	//  Replace all SVG images with inline SVG
+	// https://gist.github.com/Bloggerschmidt/61beeca2cce94a70c9df
+	//////////////////////////////////////////////////////		 
+	$('img.svg').each(function(){
+		var $img = $(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = $(data).find('svg');
+
+			// Add replaced image's ID to the new SVG
+			if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+
+		}, 'xml');
+
+});
+
 }); 
